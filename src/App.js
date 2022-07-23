392621231b8178550a8d21a4b0a5e7d24b97ac08
William M. Tsikata <tsikatawill@gmail.com>
2022-07-23 04:04:12 +0000

    :rocket: plan out layout of page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
 export default function App() {
-  return <div className="App">app</div>
+    return (
+        <div className="App">
+            <header>
+                <nav className="navbar"></nav>
+                <section className="hero"></section>
+            </header>
+            <main>
+                <section className="about" id="about"></section>
+                <section className="officers" id="officers"></section>
+                <section className="donate" id="donate"></section>
+                <section className="news" id="news"></section>
+                <section className="contact" id="contact"></section>
+            </main>
+            <footer></footer>
+        </div>
+    )
 }
