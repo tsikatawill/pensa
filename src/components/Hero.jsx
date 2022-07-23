bcbb433c0abda4f00b78f671a078a2cb6978e05c
William M. Tsikata <tsikatawill@gmail.com>
2022-07-23 07:51:04 +0000

    :rocket: add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import EventCard from './EventCard'
+import { FaArrowRight } from 'react-icons/fa'
+
+export default function Hero() {
+    return (
+        <div className="hero h-full relative">
+            <div className="container flex flex-col items-center justify-center h-[400px] text-center gap-5">
+                <h1 className="text-3xl md:text-6xl max-w-2xl leading-[3.5rem]">
+                    All you need is Living Worship
+                </h1>
+
+                <div className="text-sm max-w-md ">
+                    <p>
+                        Pentecost celebrates the blossoming of the Holy Spirit on the disciples of
+                        Jesus Christ and that is a lesson that goodness will always show the way, we
+                        only need to keep the faith!
+                    </p>
+
+                    <p className="text-right w-full text-xs mt-2">
+                        ― N.T. Wright, The Challenge of Easter
+                    </p>
+                </div>
+
+                <div className="ctas space-x-4 mt-10">
+                    <button className="btn btn-neutral text-sm inline-flex items-center gap-2">
+                        join us now <FaArrowRight />
+                    </button>
+                    <button className="btn btn-tertiary text-sm">learn more</button>
+                </div>
+
+                <div className="absolute bottom-5 w-screen overflow-x-scroll py-5 scroll-smooth">
+                    <EventCard date={{ day: '23', month: 'Jul' }} />
+                </div>
+            </div>
+        </div>
+    )
+}
