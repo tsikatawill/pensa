37f5150c38e68cb7627dd07d323ee695dbfe3c35
William M. Tsikata <tsikatawill@gmail.com>
2022-07-24 14:31:11 +0000

    :rocket: finish app full layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import AboutSection from './components/AboutSection'
+import BlogpostsSection from './components/BlogpostsSection'
+import ContactSection from './components/ContactSection'
+import DonateSection from './components/DonateSection'
+import Footer from './components/Footer'
 import Header from './components/Header'
+import OfficersSection from './components/OfficersSection'
 
 export default function App() {
     return (
         <div className="App">
             <Header />
             <main>
-                <section className="about" id="about"></section>
-                <section className="officers" id="officers"></section>
-                <section className="donate" id="donate"></section>
-                <section className="news" id="news"></section>
-                <section className="contact" id="contact"></section>
+                <AboutSection />
+                <OfficersSection />
+                <DonateSection />
+                <BlogpostsSection />
+                <ContactSection />
             </main>
-            <footer></footer>
+            <Footer />
         </div>
     )
 }
